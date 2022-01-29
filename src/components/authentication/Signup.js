7876dd3f9267f3ab7c9ba72ca880fea05eb687b5
Vlad Dragoi <vladdragoi1@gmail.com>
2022-01-29 18:14:59 +0200

    sign up form validation

--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -1,30 +1,55 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import { useAuth } from "../../context/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
+import { AiFillExclamationCircle as ErrorIco } from "react-icons/ai";
 
 export const Signup = () => {
+	const firstNameRef = useRef("");
+	const lastNameRef = useRef("");
 	const emailRef = useRef("");
 	const passwordRef = useRef("");
 	const passwordConfirmRef = useRef("");
 	const { signup } = useAuth();
-	const [error, setError] = useState("");
+	const [error, setError] = useState({});
 	const [loading, setLoading] = useState(false);
 	const navigate = useNavigate();
 
 	async function handleSubmit(e) {
 		e.preventDefault();
 
+		function nameIsValid(name) {
+			return /^[a-zA-Z]+$/.test(name);
+		}
+
 		function emailIsValid(email) {
 			return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 		}
 
+		function strongPassword(password) {
+			return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/.test(
+				password
+			);
+		}
+
+		if (!nameIsValid(firstNameRef.current.value)) {
+			return setError({ firstName: "Please provide a valid name" });
+		}
+
+		if (!nameIsValid(lastNameRef.current.value)) {
+			return setError({ lastName: "Please provide a valid name" });
+		}
+
 		if (!emailIsValid(emailRef.current.value)) {
-			return setError("Invalid email");
+			return setError({ email: "Please provide a valid Email address" });
+		}
+
+		if (!strongPassword(passwordRef.current.value)) {
+			return setError({ password: "Please provide a strong password" });
 		}
 
 		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-			return setError("Passwords do not match");
+			return setError({ password: "Passwords do not match" });
 		}
 
 		try {
@@ -33,62 +58,109 @@ export const Signup = () => {
 			await signup(emailRef.current.value, passwordRef.current.value);
 			navigate("/");
 		} catch {
-			setError("Failed to sign up");
+			setError({ failed: "Failed to sign up" });
 		}
 
 		setLoading(false);
 	}
 
 	return (
-		<div className="grid place-items-center font-card w-full h-screen">
+		<div className="grid place-items-center font-card bg-background w-full h-screen lg:justify-items-end">
 			<form
-				className="grid place-items-center w-11/12 max-w-sm bg-white shadow-3xl rounded-md px-8 pt-6 pb-8"
+				className="grid place-items-center w-11/12 max-w-sm bg-white shadow-md rounded-md px-8 py-6 lg:mr-28 lg:py-10"
 				onSubmit={handleSubmit}
 			>
 				<h1 className="text-4xl">LOGO</h1>
-				<p className="text-primary font-medium text-md my-7">
+				<p className="text-primary font-medium text-md my-7 lg:text-xl lg:my-12">
 					Sign up for a free account
 				</p>
 
 				<div className="grid grid-cols-2 gap-3">
-					<input
-						className="text-sm font-[200] bg-neutral text-gray-500 leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline"
-						type="text"
-						placeholder="First name"
-						required
-					/>
-					<input
-						className="text-sm font-[200] bg-neutral text-gray-500 leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline"
-						type="text"
-						placeholder="Last name"
-						required
-					/>
-					<input
-						className="col-span-2  text-sm font-[200] bg-neutral text-gray-500 leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline"
-						type="text"
-						placeholder="Email address"
-						required
-						ref={emailRef}
-					/>
-					<input
-						className="col-span-2  text-sm font-[200] bg-neutral text-gray-500 leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline"
-						type="password"
-						placeholder="Create password"
-						required
-						ref={passwordRef}
-					/>
-					<input
+					<div className="self-end relative">
+						{error.firstName && (
+							<>
+								<div className="text-red-400 text-[0.6rem] italic font-light mb-2">
+									{error.firstName}
+								</div>
+								<ErrorIco className="absolute right-1 bottom-2 text-red-400" />
+							</>
+						)}
+						<input
+							className={`text-sm font-[200] bg-neutral text-gray-500 placeholder-[#c4c4c4] leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline ${
+								error.firstName && "border border-red-400"
+							}`}
+							type="text"
+							placeholder="First name"
+							required
+							ref={firstNameRef}
+						/>
+					</div>
+					<div className="self-end relative">
+						{error.lastName && (
+							<>
+								<div className="text-red-400 text-[0.6rem] italic font-light mb-2">
+									{error.lastName}
+								</div>
+								<ErrorIco className="absolute right-1 bottom-2 text-red-400" />
+							</>
+						)}
+						<input
+							className={`text-sm font-[200] bg-neutral text-gray-500 placeholder-[#c4c4c4] leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline ${
+								error.lastName && "border border-red-400"
+							}`}
+							type="text"
+							placeholder="Last name"
+							required
+							ref={lastNameRef}
+						/>
+					</div>
+					<div className="col-span-2 relative">
+						{error.email && (
+							<>
+								<div className="text-red-400 text-[0.6rem] italic font-light mb-2">
+									{error.email}
+								</div>
+								<ErrorIco className="absolute right-1 bottom-2 text-red-400" />
+							</>
+						)}
+						<input
+							className={`text-sm font-[200] bg-neutral text-gray-500 placeholder-[#c4c4c4] leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline lg:my-2 ${
+								error.email && "border border-red-400"
+							}`}
+							type="text"
+							placeholder="Email address"
+							required
+							ref={emailRef}
+						/>
+					</div>
+					<div className="col-span-2 relative">
+						{error.password && (
+							<>
+								<div className="text-red-400 text-[0.6rem] italic font-light mb-2">
+									{error.password}
+								</div>
+								<ErrorIco className="absolute right-1 bottom-2 text-red-400" />
+							</>
+						)}
+						<input
+							className={`text-sm font-[200] bg-neutral text-gray-500 placeholder-[#c4c4c4] leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline ${
+								error.password && "border border-red-400"
+							}`}
+							type="password"
+							placeholder="Create password"
+							required
+							ref={passwordRef}
+						/>
+					</div>
+					{/* <input
 						className="col-span-2  text-sm font-[200] bg-neutral text-gray-500 leading-tight appearance-none rounded w-full py-1 px-3 focus:outline-none focus:shadow-outline"
 						type="password"
 						placeholder="Confirm password"
 						required
 						ref={passwordConfirmRef}
-					/>
-
-					{error && <p className="text-red-500 text-xs italic">{error}</p>}
-
+					/> */}
 					<button
-						className="bg-primary text-white font-light col-span-2 justify-self-center w-48 py-1 px-4 my-10 rounded-md cursor-pointer focus:outline-none focus:shadow-outline"
+						className="bg-primary text-white font-light col-span-2 justify-self-center w-48 py-1 px-4 my-10 rounded-md cursor-pointer focus:outline-none focus:shadow-outline lg:text-sm lg:mb-4 lg:py-2"
 						type="submit"
 						disabled={loading}
 					>
@@ -106,7 +178,7 @@ export const Signup = () => {
 					</Link>
 				</p>
 
-				<p className="text-primary font-light text-xs mt-20">
+				<p className="text-primary font-light text-xs mt-20 lg:mt-12">
 					by creating an account you will receive 10 crypto
 				</p>
 			</form>
